ec71e6720ae7e2dc795824519bf0b55519d74476
felipeam10 <felipeam10@hotmail.com>
2024-10-10 17:35:45 -0300

    consuming REST API

--- cypress-express-mark/cypress/e2e/tasks.cy.js
+++ cypress-express-mark/cypress/e2e/tasks.cy.js
@@ -1,13 +1,22 @@
 /// <reference types="cypress" />
 
 describe('tarefas', () => {
+    
     it('deve cadastrar uma nova tarefa', () => {
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: {
+                name: 'Tarefa de teste'
+            }
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+        
         cy.visit('http://localhost:3000')
     
         cy.get('input[placeholder="Add a new Task"]')
-            .type('Ler um livro de node.js')
-
-        ////button[contains(text(), "Create")]
+        .type('Tarefa de teste')    
 
         cy.contains('button', 'Create').click()
     })
