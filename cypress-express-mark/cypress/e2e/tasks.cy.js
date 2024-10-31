9fa46355d79f851c17e49ef407b5f886654429eb
felipeam10 <felipeam10@hotmail.com>
2024-10-31 17:20:25 -0300

    custom commands

--- cypress-express-mark/cypress/e2e/tasks.cy.js
+++ cypress-express-mark/cypress/e2e/tasks.cy.js
@@ -6,22 +6,9 @@ describe('tarefas', () => {
         
         const taskName = 'Tarefa de teste'
         
-        cy.request({
-            url: 'http://localhost:3333/helper/tasks',
-            method: 'DELETE',
-            body: {
-                name: taskName
-            }
-        }).then(response => {
-            expect(response.status).to.eq(204)
-        })
+        cy.deleteTaskByName(taskName)
         
-        cy.visit('http://localhost:3000')
-    
-        cy.get('input[placeholder="Add a new Task"]')
-        .type(taskName)    
-
-        cy.contains('button', 'Create').click()
+        cy.createTask(taskName)
 
         cy.contains('main div p', taskName)
             .should('be.visible')
@@ -34,34 +21,12 @@ describe('tarefas', () => {
             is_done: false
         }
         
-        cy.request({
-            url: 'http://localhost:3333/helper/tasks',
-            method: 'DELETE',
-            body: {
-                name: taskName1.name
-            }
-        }).then(response => {
-            expect(response.status).to.eq(204)
-        })
+        cy.deleteTaskByName(taskName1.name)
 
-
-        //Dado que eu tenha uma tarefa duplicada
-        cy.request({
-            url: 'http://localhost:3333/tasks',
-            method: 'POST',
-            body: taskName1
-        }).then(response => {
-            expect(response.status).to.eq(201)
-        })
+        cy.postTask(taskName1)    
         
-        
-        //Quando faço o cadastro dessa tarefa
-        cy.visit('http://localhost:3000')
-        cy.get('input[placeholder="Add a new Task"]')
-            .type(taskName1.name)    
-        cy.contains('button', 'Create').click()
+        cy.createTask(taskName1.name)
 
-        //Então devo ver uma mensagem de duplicidade
         cy.get('.swal2-html-container')
             .should('be.visible')
             .should('have.text', 'Task already exists!')
