c6951a5782a9af007d2bed34a93d2b7493f089a1
felipeam10 <felipeam10@hotmail.com>
2024-10-31 15:51:46 -0300

    independent tests

--- cypress-express-mark/cypress/e2e/tasks.cy.js
+++ cypress-express-mark/cypress/e2e/tasks.cy.js
@@ -23,5 +23,42 @@ describe('tarefas', () => {
         cy.contains('main div p', 'Tarefa de teste')
             .should('be.visible')
     })
+
+    it('nao deve permitir uma tarefa duplicada', () => {
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: {
+                name: 'Learn SpringBoot with Java'
+            }
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+
+
+        //Dado que eu tenha uma tarefa duplicada
+        cy.request({
+            url: 'http://localhost:3333/tasks',
+            method: 'POST',
+            body: {
+                name: 'Learn SpringBoot with Java',
+                is_done: false
+            }
+        }).then(response => {
+            expect(response.status).to.eq(201)
+        })
+        
+        
+        //Quando faço o cadastro dessa tarefa
+        cy.visit('http://localhost:3000')
+        cy.get('input[placeholder="Add a new Task"]')
+            .type('Learn SpringBoot with Java')    
+        cy.contains('button', 'Create').click()
+
+        //Então devo ver uma mensagem de duplicidade
+        cy.get('.swal2-html-container')
+            .should('be.visible')
+            .should('have.text', 'Task already exists!')
+    })
     
 })
