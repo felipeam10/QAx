c6d1a8c3e6dcb2c42004ceb50dd90e8cafa4cbdc
felipeam10 <felipeam10@hotmail.com>
2024-11-04 14:32:43 -0300

    organization by context

--- cypress-express-mark/cypress/e2e/tasks.cy.js
+++ cypress-express-mark/cypress/e2e/tasks.cy.js
@@ -2,40 +2,48 @@
 
 describe('tarefas', () => {
   
-    it('deve cadastrar uma nova tarefa', () => {
+    context('cadastro', () => {
+          
+        it('deve cadastrar uma nova tarefa', () => {
+    
+            const taskName = 'Tarefa de teste'
+            
+            cy.deleteTaskByName(taskName)
+            
+            cy.createTask(taskName)
+    
+            cy.contains('main div p', taskName)
+                .should('be.visible')
+        })
+    
+        it('nao deve permitir uma tarefa duplicada', () => {
+            
+            const taskName1 = {
+                name: 'Learn SpringBoot with Java',
+                is_done: false
+            }
+            
+            cy.deleteTaskByName(taskName1.name)
+    
+            cy.postTask(taskName1)    
+            
+            cy.createTask(taskName1.name)
+    
+            cy.get('.swal2-html-container')
+                .should('be.visible')
+                .should('have.text', 'Task already exists!')
+        })
+    
+        it('campo obrigatório', () => {
+            cy.createTask()
+            cy.isRequired('This is a required field')
+            
+        })
         
-        const taskName = 'Tarefa de teste'
-        
-        cy.deleteTaskByName(taskName)
-        
-        cy.createTask(taskName)
-
-        cy.contains('main div p', taskName)
-            .should('be.visible')
     })
+      
+    
 
-    it('nao deve permitir uma tarefa duplicada', () => {
-        
-        const taskName1 = {
-            name: 'Learn SpringBoot with Java',
-            is_done: false
-        }
-        
-        cy.deleteTaskByName(taskName1.name)
 
-        cy.postTask(taskName1)    
-        
-        cy.createTask(taskName1.name)
-
-        cy.get('.swal2-html-container')
-            .should('be.visible')
-            .should('have.text', 'Task already exists!')
-    })
-
-    it('campo obrigatório', () => {
-        cy.createTask()
-        cy.isRequired('This is a required field')
-        
-    })
     
 })
